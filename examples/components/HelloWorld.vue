ecb3f05954d707b824c0b2291abf0a22c3ea8333
xiumin shi <shixm_1102@qq.com>
2021-06-09 19:17:58 +0800

    develop plugin

--- examples/components/HelloWorld.vue
+++ examples/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <aometamask />
+    <shixmetamask />
   </div>
 </template>
 
--- examples/main.js
+++ examples/main.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import aometamask from "../packages/index";
+import shixmetamask from "../packages/index";
 import i18n from "./i18n";
 Vue.config.productionTip = false;
-Vue.use(aometamask);
+Vue.use(shixmetamask);
 
 new Vue({
   i18n,
