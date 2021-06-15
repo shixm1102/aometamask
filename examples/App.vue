29ab5e7902ad469c4370cb3de75432d28137347e
xiumin shi <shixm_1102@qq.com>
2021-06-15 11:05:16 +0800

    examples

--- examples/App.vue
+++ examples/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <aometamask />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import { ethers, BigNumber } from "ethers";
+import { BigNumber } from "ethers";
+
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
   data() {
     return {
       provider: null,
@@ -25,82 +22,16 @@ export default {
       ],
     };
   },
-  created() {
-    this.connect();
-  },
+  created() {},
   methods: {
-    async connect() {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
+    sendTx() {
+      const amount = "92675224800514997";
+      console.log(BigNumber.from(amount).toHexString());
+      this.$metamask.sendTx({
+        from: "0x6c999dbc796102774E7CF2b45eD9097a8C0F4d7A",
+        to: "0x867f1469356D37313406b75c461fA057c829c749",
+        value: BigNumber.from(amount).toHexString(),
       });
-      setTimeout(() => {
-        // A Web3Provider wraps a standard Web3 provider, which is
-        // what Metamask injects as window.ethereum into each page
-        // const provider = new ethers.providers.Web3Provider(window.ethereum);
-        // The Metamask plugin also allows signing transactions to
-        // send ether and pay to change state within the blockchain.
-        // For this, you need the account signer...
-        // const signer = provider.getSigner();
-        // console.log("signer", signer);
-        // signer.signMessage("hello").then(console.log);
-        // const abi = [
-        //   // Some details about the token
-        //   "function name() view returns (string)",
-        //   "function symbol() view returns (string)",
-        //   // Get the account balance
-        //   "function balanceOf(address) view returns (uint)",
-        //   // Send some of your tokens to someone else
-        //   "function transfer(address to, uint amount)",
-        //   // An event triggered whenever anyone transfers to someone else
-        //   "event Transfer(address indexed from, address indexed to, uint amount)",
-        // ];
-        // this.sendContract(
-        //   "0x5595ad3ec993bdb978d4a182d913a29ffcc0b5a9",
-        //   abi,
-        //   "transfer",
-        //   ["0x6c999dbc796102774E7CF2b45eD9097a8C0F4d7A", "0xff"]
-        // ).then((txHash) => {
-        //   console.log("txHash", txHash);
-        //   provider.once(txHash.hash, (transaction) => {
-        //     // Emitted when the transaction has been mined
-        //     console.log("transaction", transaction);
-        //   });
-        // });
-      }, 5000);
-      const chainId = await this.getChainId();
-      const balance = await this.getBalance(accounts[0]);
-      console.log("chainId", chainId);
-      console.log("balance", balance);
-    },
-    async callContract(contractAddress, abi, name, params) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      // The Contract object
-      const contract = new ethers.Contract(contractAddress, abi, provider);
-      if (params) return await contract[name](...params);
-      return await contract[name]();
-    },
-
-    async sendContract(contractAddress, abi, name, params) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      // The Contract object
-      const contract = new ethers.Contract(contractAddress, abi, provider);
-      const contractWithSigner = contract.connect(signer);
-      if (params) return await contractWithSigner[name](...params);
-      await contractWithSigner[name]();
-    },
-
-    async getChainId() {
-      const chainId = await window.ethereum.request({ method: "eth_chainId" });
-
-      return BigNumber.from(chainId).toNumber();
-    },
-    async getBalance(address) {
-      const balance = await window.ethereum.request({
-        method: "eth_getBalance",
-        params: [address, "latest"],
-      });
-      return BigNumber.from(balance).toString();
     },
   },
 };
